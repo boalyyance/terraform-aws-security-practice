925db6f47bbd1e16cdc0d6b0d8c675374a6c1c5d
Boaly Yance <boalyyance@users.noreply.github.com>
2026-09-14 13:02:42 -0500

    Delete terraform-aws-security-practice directory

--- terraform-aws-security-practice/05-terraform-modules/modules/secure-s3-bucket/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_arn" {
-  description = "ARN of the created bucket"
-  value       = aws_s3_bucket.this.arn
-}
-
-output "bucket_id" {
-  description = "ID (name) of the created bucket"
-  value       = aws_s3_bucket.this.id
-}
